87d01f89cc11e69fa73b7dc88e3732e045cc3823
Pavel Tarakanov <pavel191094@icloud.com>
2024-11-15 10:57:12 +0300

    set up `get_product_cache_repository()` in dependencies

--- dependencies.py
+++ dependencies.py
@@ -2,7 +2,9 @@ from fastapi import Depends
 
 from sqlalchemy.orm import Session
 
+from infrastructure.cache import get_redis_connection
 from infrastructure.database import get_db_session
+from repositories.product_cache_repository import ProductCache
 from repositories.product_repository import ProductRepository
 from services.product_service import ProductService
 
@@ -11,11 +13,16 @@ def get_product_repository(db_session: Session = Depends(get_db_session)) -> Pro
     return ProductRepository(db_session)
 
 
+def get_product_cache_repository() -> ProductCache:
+    redis_connection = get_redis_connection()
+    return ProductCache(redis_connection)
+
+
 def get_product_service(
     product_repository: ProductRepository = Depends(get_product_repository),
-    # product_cache: ProductCache = Depends(get_products_cache_repository)
+    product_cache: ProductCache = Depends(get_product_cache_repository)
 ) -> ProductService:
     return ProductService(
-        product_repository=product_repository
-        # product_cache=product_cache
+        product_repository=product_repository,
+        product_cache=product_cache
     )
