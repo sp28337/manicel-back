e59b17ed073dbcf4dd9852697b39f474d95874c8
Pavel Tarakanov <pavel191094@icloud.com>
2024-11-18 15:56:13 +0300

    handle errors

--- handlers/product_handlers.py
+++ handlers/product_handlers.py
@@ -1,14 +1,15 @@
 from typing import Annotated, List
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 
-from dependencies import get_product_service
-from schemas import ProductsSchema, ProductSchema, CreateProductSchema, UpdtaeProductSchema
+from dependencies import get_product_service, get_request_user_id, get_request_admin
+from exceptions import ProductAlreadyExistsException, ProductNotFoundException
+from schemas import ProductSchema, CreateProductSchema, UpdtaeProductSchema
 from services import ProductService
 
 router = APIRouter(prefix="/products", tags=["products"])
 
 
-@router.get(path="/all", response_model=List[ProductsSchema])
+@router.get(path="/all", response_model=List[ProductSchema])
 async def read_products(
     product_service: Annotated[ProductService, Depends(get_product_service)]
 ):
@@ -20,30 +21,60 @@ async def read_product(
     product_id: int,
     product_service: Annotated[ProductService, Depends(get_product_service)]
 ):
-    new_product: ProductSchema = product_service.read_product(product_id)
-    return new_product
+    try:
+        new_product: ProductSchema = product_service.read_product(product_id)
+        return new_product
+    except ProductNotFoundException as e:
+        raise HTTPException(
+            status_code=404,
+            detail=e.detail
+        )
 
 
 @router.post(path="/", response_model=ProductSchema)
 async def create_product(
     body: CreateProductSchema,
-    product_service: Annotated[ProductService, Depends(get_product_service)]
+    product_service: Annotated[ProductService, Depends(get_product_service)],
+    is_admin: Annotated[bool, Depends(get_request_admin)]
+    # user_id: Annotated[int, Depends(get_request_user_id)]
 ):
-    return product_service.create_product(body)
+    try:
+        return product_service.create_product(body)
+    except ProductAlreadyExistsException as e:
+        raise HTTPException(
+            status_code=403,
+            detail=e.detail
+        )
 
 
 @router.patch(path="/{product_id}", response_model=UpdtaeProductSchema)
 async def update_product_name(
     product_id: int,
     product_name: str,
-    product_service: Annotated[ProductService, Depends(get_product_service)]
+    product_service: Annotated[ProductService, Depends(get_product_service)],
+    # user_id: Annotated[int, Depends(get_request_user_id)],
+    is_admin: Annotated[bool, Depends(get_request_admin)]
 ):
-    return product_service.update_product_name(product_id, product_name)
+    try:
+        return product_service.update_product_name(product_id, product_name)
+    except ProductNotFoundException as e:
+        raise HTTPException(
+            status_code=404,
+            detail=e.detail
+        )
 
 
-@router.delete(path="/{product_id}")
+@router.delete(path="/{product_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_product(
     product_id: int,
-    product_service: Annotated[ProductService, Depends(get_product_service)]
+    product_service: Annotated[ProductService, Depends(get_product_service)],
+    is_admin: Annotated[bool, Depends(get_request_admin)]
+    # user_id: Annotated[int, Depends(get_request_user_id)]
 ):
-    return product_service.delete_product(product_id)
+    try:
+        return product_service.delete_product(product_id)
+    except ProductNotFoundException as e:
+        raise HTTPException(
+            status_code=404,
+            detail=e.detail
+        )
--- services/product_service.py
+++ services/product_service.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+from exceptions import ProductNotFoundException, ProductAlreadyExistsException
 from repositories import ProductCache, ProductRepository
 from schemas import ProductSchema, CreateProductSchema, UpdtaeProductSchema
 
@@ -52,13 +53,26 @@ class ProductService:
         return product_schema
 
     def create_product(self, body: CreateProductSchema) -> ProductSchema:
+        check_product = self.product_repository.read_product_by_name(body.name)
+        if check_product:
+            raise ProductAlreadyExistsException
+
         product_id = self.product_repository.create_product(body)
         new_product = self.product_repository.read_product_by_id(product_id)
         return ProductSchema.model_validate(new_product)
 
     def update_product_name(self, product_id: int, product_name: str) -> UpdtaeProductSchema:
+        check_product = self.product_repository.read_product_by_id(product_id)
+
+        if check_product is None:
+            raise ProductNotFoundException
+
         updated_product = self.product_repository.update_product_name(product_id, product_name)
         return UpdtaeProductSchema.model_validate(updated_product)
 
     def delete_product(self, product_id: int) -> None:
+        check_product = self.product_repository.read_product_by_id(product_id)
+
+        if check_product is None:
+            raise ProductNotFoundException
         self.product_repository.delete_product(product_id)
