1bb41a7681c19a25e4b310dbf2337a82fc0fa8cb
Pavel Tarakanov <pavel191094@icloud.com>
2024-12-03 13:32:43 +0300

    add mail client

--- app/user/auth/clients/__init__.py
+++ app/user/auth/clients/__init__.py
@@ -1,5 +1,5 @@
 from app.user.auth.clients.google import GoogleClient
 from app.user.auth.clients.yandex import YandexClient
+from app.user.auth.clients.mail import MailClient
 
-
-__all__ = ["GoogleClient", "YandexClient"]
+__all__ = ["GoogleClient", "YandexClient", "MailClient"]
