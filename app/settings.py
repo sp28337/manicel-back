d5ea8ffbb51b4fbe6bf0f148b7734171d3eed47c
Pavel Tarakanov <pavel191094@icloud.com>
2024-12-04 11:34:48 +0300

    remove celery
    Signed-off-by: Pavel Tarakanov <pavel191094@icloud.com>

--- app/settings.py
+++ app/settings.py
@@ -27,10 +27,6 @@ class Settings(BaseSettings):
     YANDEX_TOKEN_URI: str = "https://oauth.yandex.ru/token"
 
     CELERY_BROKER_URL: str = "amqp://guest:guest@localhost:5672//"
-    SMTP_FROM_EMAIL: str = "tarakanov191094@yandex.ru"
-    SMTP_PORT: int = 465
-    SMTP_HOST: str = "smtp.yandex.ru"
-    SMTP_PASSWORD: str = ""
 
     model_config = {
         "env_file": ".local.env"
--- app/user/auth/clients/mail.py
+++ app/user/auth/clients/mail.py
@@ -1,9 +1,8 @@
-from worker.celery import send_email_task
+import aio_pika
 
 
 class MailClient:
 
     @staticmethod
-    def send_welcome_email(to: str):
-        task_id = send_email_task.delay(f"Welcome email", f"Welcom to MANICEL", to)
-        return task_id
+    def send_welcome_email(to: str) -> None:
+        pass
