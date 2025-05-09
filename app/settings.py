709cb2f5ecd0ac3ac1db9257e58dbc03fabbbad3
Pavel Tarakanov <pavel191094@icloud.com>
2025-05-09 16:05:50 +0300

    fix settings

--- app/settings.py
+++ app/settings.py
@@ -2,12 +2,12 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DB_HOST: str = ""
-    DB_PORT: int = 7777
-    DB_USER: str = ""
-    DB_PASSWORD: str = ""
-    DB_DRIVER: str = ""
-    DB_NAME: str = ""
+    DB_HOST: str = "db"
+    DB_PORT: int = 5432
+    DB_USER: str = "postgres"
+    DB_PASSWORD: str = "password"
+    DB_DRIVER: str = "postgresql+asyncpg"
+    DB_NAME: str = "postgres"
 
     CACHE_HOST: str = "redis"
     CACHE_PORT: int = 6379
