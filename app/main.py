23837726e354573b5ff649dc4849c47c2e84730f
Pavel Tarakanov <pavel191094@icloud.com>
2025-05-18 15:16:32 +0300

    add ping handlers

--- app/main.py
+++ app/main.py
@@ -2,13 +2,16 @@ import logging
 
 
 from contextlib import asynccontextmanager
+from typing import Annotated
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.product.handlers import router as product_router
 from app.user.handlers import router as user_router
 from app.user.auth.handlers import router as auth_router
+from app.dependencies import get_product_repository
+from app.product.repository import ProductRepository
 
 routers = [product_router, user_router, auth_router]
 
@@ -36,3 +39,13 @@ app.add_middleware(
 
 for router in routers:
     app.include_router(router)
+
+
+@app.get("/app/ping")
+async def ping_app():
+    return {"text": "app is working"}
+
+
+@app.get("/db/ping")
+async def ping_db(task_repository: Annotated[ProductRepository, Depends(get_product_repository)]):
+    await task_repository.ping_db()
