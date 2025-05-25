4f7f4afde69b296f98ec4765c2611152d16ec089
Pavel Tarakanov <pavel191094@icloud.com>
2025-05-25 10:31:17 +0300

    set Base class

--- app/infrastructure/orm_base.py
+++ app/infrastructure/orm_base.py
@@ -1,7 +1,11 @@
+import sqlalchemy
+import enum
+
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.ext.asyncio import AsyncAttrs
 
 
 class Base(AsyncAttrs, DeclarativeBase):
-
-    pass
+    type_annotation_map = {
+        enum.Enum: sqlalchemy.Enum(enum.Enum, native_enum=False)
+    }
